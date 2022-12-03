38493f7ec3c9179ccfde96d912c362a6395b6b6e
Kirill Buckovsky <kbutskovskiy@mail.ru>
2022-12-03 03:49:31 +0300

    Add def resist() and header

--- capasitor/main/views.py
+++ capasitor/main/views.py
@@ -16,17 +16,36 @@ def voltage(array, a, c, tim):
         t = t + 0.1
 
 
-someArray = []
+def resist(array, a):
+    t = 0
+    for i in range(100):
+        array.append(a * math.sin(100 * t))
+        t = t + 0.1
+
+
+capacityArray = []
+resistArray = []
 tim = []
-voltage(someArray, 100, 10e-6, tim)
+voltage(capacityArray, 100, 10e-6, tim)
+resist(resistArray, 100)
 
-plt.plot(tim, someArray)
+plt.plot(tim, capacityArray)
 
-plt.title("Line graph")
-plt.ylabel('Y axis')
-plt.xlabel('X axis')
-fig = Figure(figsize=(10,10))
-plt.savefig('bank_data.png')
+plt.title("На конденсаторе")
+plt.ylabel('Voltage')
+plt.xlabel('Time')
+fig1 = Figure(figsize=(10, 10))
+plt.savefig('voltageOnCapacitor.png')
+plt.close(fig1)
+
+plt.plot(tim, resistArray)
+
+plt.title("На резисторе")
+plt.ylabel('Voltage')
+plt.xlabel('Time')
+fig2 = Figure(figsize=(10, 10))
+plt.savefig('voltageOnResistor.png')
+
 
 def index(request):
     error = ''
@@ -44,9 +63,10 @@ def index(request):
     }
     return render(request, 'main/index.html', data)
 
-html_str = mpld3.fig_to_html(fig)
-Html_file = open("index.html","w")
-Html_file.write(html_str)
-Html_file.close()
+
+# html_str = mpld3.fig_to_html(fig)
+# Html_file = open("index.html","w")
+# Html_file.write(html_str)
+# Html_file.close()
 def about(request):
     return render(request, 'main/about.html')
